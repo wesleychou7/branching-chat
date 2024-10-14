ab7180896c1e01c24a658ebe326abe1f03446b83
Wesley Chou <wesleychou9851@gmail.com>
2024-10-13 23:52:04 -0700

    fix slow code rendering + support for most languages

--- twig-app/app/components/messages/CodeHighlighter.tsx
+++ twig-app/app/components/messages/CodeHighlighter.tsx
@@ -1,14 +1,36 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 import Box from "@mui/joy/Box";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
 import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 import { useState } from "react";
+import { Refractor } from "react-refractor";
+import { registerAllLanguages } from "./languages";
+import "prism-themes/themes/prism-vsc-dark-plus.css";
+import "./CodeHighlighter.css";
+import { ErrorBoundary } from "react-error-boundary";
+
+registerAllLanguages();
 
 interface Props {
   language: string;
   code: string;
 }
+
+// code to render if language is not supported/specified
+const TextCode = ({ code, className }: { code: string; className: string }) => {
+  return (
+    <code
+      className={className}
+      style={{
+        backgroundColor: "#eeee",
+        padding: "4px",
+        borderRadius: "5px",
+        fontSize: "14px",
+      }}
+    >
+      {code}
+    </code>
+  );
+};
 
 const CodeHighlighter = ({ language, code }: Props) => {
   const [copyButtonClicked, setCopyButtonClicked] = useState<boolean>(false);
@@ -35,68 +57,50 @@ const CodeHighlighter = ({ language, code }: Props) => {
         {code}
       </code>
     );
-  } else {
-    return (
-      <Box maxWidth="750px" position="relative">
-        <Box
-          color="#e6e6e6"
-          bgcolor="#3e3e42"
-          py={1}
-          px={1.5}
-          sx={{ borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Box>{language}</Box>
-
-          <button
-            onClick={onCopyClick}
-            style={{
-              padding: 0,
-              backgroundColor: "transparent",
-              border: "none",
-              cursor: "pointer",
-            }}
-          >
-            {copyButtonClicked ? (
-              <CheckRoundedIcon
-                fontSize="small"
-                sx={{ width: 17, height: "auto", color: "#e6e6e6" }}
-              />
-            ) : (
-              <ContentCopyRoundedIcon
-                fontSize="small"
-                sx={{ width: 17, height: "auto", color: "#e6e6e6" }}
-              />
-            )}
-          </button>
-        </Box>
-        <SyntaxHighlighter
-          language={language}
-          style={vscDarkPlus}
-          customStyle={{
-            margin: 0,
-            fontSize: "14px",
-            lineHeight: "23px",
-            borderBottomLeftRadius: "10px",
-            borderBottomRightRadius: "10px",
-            maxWidth: "750px",
-            overflow: "auto",
-            background: "#252526",
-          }}
-          codeTagProps={{
-            style: {
-              fontSize: "inherit",
-              lineHeight: "inherit",
-            },
-          }}
-        >
-          {code.replace(/\n$/, "")}
-        </SyntaxHighlighter>
-      </Box>
-    );
   }
+
+  return (
+    <Box maxWidth="750px" position="relative">
+      <Box
+        color="#e6e6e6"
+        bgcolor="#3e3e42"
+        py={1}
+        px="15px"
+        sx={{ borderTopLeftRadius: "10px", borderTopRightRadius: "10px" }}
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box>{language}</Box>
+
+        <button
+          onClick={onCopyClick}
+          style={{
+            padding: 0,
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+          }}
+        >
+          {copyButtonClicked ? (
+            <CheckRoundedIcon
+              fontSize="small"
+              sx={{ width: 17, height: "auto", color: "#e6e6e6" }}
+            />
+          ) : (
+            <ContentCopyRoundedIcon
+              fontSize="small"
+              sx={{ width: 17, height: "auto", color: "#e6e6e6" }}
+            />
+          )}
+        </button>
+      </Box>
+      
+      <ErrorBoundary fallback={<TextCode code={code} className="refractor" />}>
+        <Refractor language={language} value={code} className="refractor" />
+      </ErrorBoundary>
+    </Box>
+  );
 };
 
 export default CodeHighlighter;
