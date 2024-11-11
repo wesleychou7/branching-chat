031214b90cce4a93e4e950b5c83ed4b657c95e2e
Wesley Chou <wesleychou9851@gmail.com>
2024-11-11 00:28:05 -0800

    new supabase project because old one stopped working after unpausing

--- twig-app/app/supabase.ts
+++ twig-app/app/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qucsourpednssdmakokz.supabase.co'
+const supabaseUrl = 'https://qrpwsmzxaxksgvkvmxwv.supabase.co'
 // const supabaseKey = process.env.SUPABASE_KEY || ''
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1Y3NvdXJwZWRuc3NkbWFrb2t6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxODM5NzQsImV4cCI6MjA0NDc1OTk3NH0.OiWSRCHDZHwq0Zl56x1Pmk9sptoZPIAFqUXmnRWB5bA'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFycHdzbXp4YXhrc2d2a3ZteHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwOTA1MjYsImV4cCI6MjA0NjY2NjUyNn0.LJd2-daGnChGMgoX6dC2R9SJDbf59pbNtXEhTUhjY9Y'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
