6d9fe64be2897c7776931ce29834b730adbb4d21
Wesley Chou <wesleychou9851@gmail.com>
2025-01-19 15:04:03 -0800

    add vercel analytics

--- twig-app/app/layout.tsx
+++ twig-app/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { store } from "@/app/store";
 import { Provider } from "react-redux";
 import "./styles.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
     <Provider store={store}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}<Analytics /></body>
       </html>
     </Provider>
   );
